7fb201b710dfe9e861c9b9a0dea0160892b33a11
xidian <vaielx@163.com>
2019-03-08 10:38:50 +0800

    test

---
+++ outdd-read-oauth-centre/src/main/java/com/outdd/outddreadoauthcentre/OutddReadOauthCentreApplication.java
@@ -0,0 +1,13 @@
+package com.outdd.outddreadoauthcentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutddReadOauthCentreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutddReadOauthCentreApplication.class, args);
+    }
+
+}
